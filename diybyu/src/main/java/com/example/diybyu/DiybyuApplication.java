b6c0f74e5752af6a321a9fa1bd2239355b164745
FraBordeaux <francesca.nadel@gmail.com>
2023-01-24 15:03:42 +0100

    ready to go

---
+++ diybyu/src/main/java/com/example/diybyu/DiybyuApplication.java
@@ -0,0 +1,13 @@
+package com.example.diybyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiybyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiybyuApplication.class, args);
+	}
+
+}
